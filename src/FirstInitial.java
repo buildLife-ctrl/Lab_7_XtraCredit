0848aec99a3b5ec1659ac26a9fd96af5db9d82c0
617998 <sinchuri.617998@swl.k12.oh.us>
2024-10-21 09:44:58 -0400

    2ini

--- src/FirstInitial.java
+++ src/FirstInitial.java
@@ -1,5 +1,63 @@
-public class Main {
+import java.util.Scanner;
+
+public class FirstInitial {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        //first line, top of s
+        for (int i = 0; i < 2; i++) {
+            System.out.print(" ");
+        }
+        for (int i = 0; i < 4; i++) {
+            System.out.print("*");
+        }
+        System.out.println(); //2nd line and so on
+        for (int i = 0; i < 8; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+        for (int i = 0; i < 3; i++) {
+            System.out.print("*");
+        }
+        for (int i = 0; i < 3; i++) {
+            System.out.print(" ");
+        }
+        for (int i = 0; i < 3; i++) {
+            System.out.print("*");
+        }
+        System.out.println(); //middle part of s
+        for (int i = 0; i < 2; i++) {
+            System.out.print(" ");
+        }
+        for (int i = 0; i < 3; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+        for (int i = 0; i < 5; i++) {
+            System.out.print(" ");
+        }
+        for (int i = 0; i < 3; i++) {
+            System.out.print("*");
+        }
+        System.out.println(); //bottom of s
+        for (int i = 0; i < 3; i++) {
+            System.out.print("*");
+        }
+        for (int i = 0; i < 3; i++) {
+            System.out.print(" ");
+        }
+        for (int i = 0; i < 3; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+        for (int i = 0; i < 8; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+        for (int i = 0; i < 2; i++) {
+            System.out.print(" ");
+        }
+        for (int i = 0; i < 4; i++) {
+            System.out.print("*");
+        }
     }
 }
